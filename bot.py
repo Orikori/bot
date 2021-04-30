ecfe4b8b8bb9cc5bd0310b46a1a6715f7a6d746f
Дмитрий Артемьев <d.artemev@qsoft.ru>
2021-04-30 19:02:29 +0300

    "Завтра будет  "

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@
 
 import pyowm
 from pyowm.utils.config import get_default_config
+from pyowm.utils import timestamps
 import telebot
 
 bot = telebot.TeleBot("1750857579:AAFiTDZh5-vs1qY3YdxsU-0WWs70li3NnoE")
@@ -17,8 +18,12 @@ def send(message):
     w = observation.weather
     temperature = w.temperature('celsius')['temp']
 
+    forecast = mgr.forecast_at_place(message.text, '3h')
+    answerforecast = forecast.will_be_clear_at(timestamps.tomorrow())
+
     answer = "В городе " + message.text + " сейчас \"" + w.detailed_status.title() + "\"\n"
     answer += "Температура в районе " + str(round(temperature)) + "\n"
+    answer += "Завтра будет  " + str(answerforecast) + "\n"
 
     bot.send_message(message.chat.id, answer)
 
